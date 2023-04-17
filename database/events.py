ee26ba89f9e09cd76e331a2c953f20412e7b647e
s1veme <kpdpav@gmail.com>
2023-04-17 11:00:58 +0300

    add(database): add create table in connect_to_db

--- database/events.py
+++ database/events.py
@@ -1,25 +1,34 @@
+import sqlalchemy
 from fastapi import FastAPI
 
 from databases import Database
 from loguru import logger
 
 from core.settings import AppSettings
+from database.models import BaseModel
 
 
 async def connect_to_db(app: FastAPI, settings: AppSettings) -> None:
-    logger.info("Connecting to PostgreSQL")
+    logger.info('Connecting to sqlite')
 
     db = Database(settings.database_url)
     await db.connect()
 
+    metadata = BaseModel.metadata
+    dialect = sqlalchemy.dialects.sqlite.dialect()
+    for table in metadata.tables.values():
+        schema = sqlalchemy.schema.CreateTable(table, if_not_exists=True)
+        query = str(schema.compile(dialect=dialect))
+        await db.execute(query=query)
+
     app.state.pool = db
 
-    logger.info("Connection established")
+    logger.info('Connection established')
 
 
 async def close_db_connection(app: FastAPI) -> None:
-    logger.info("Closing connection to database")
+    logger.info('Closing connection to database')
 
     await app.state.pool.disconnect()
 
-    logger.info("Connection closed")
+    logger.info('Connection closed')
