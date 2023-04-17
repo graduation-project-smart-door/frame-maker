4c4c346f0616be0530e9734c4c0c4afca42e11b9
s1veme <kpdpav@gmail.com>
2023-04-17 18:39:57 +0300

    fix(api): fix status code for create_video, now return 201

--- api/video.py
+++ api/video.py
@@ -3,6 +3,7 @@ import uuid
 
 import requests
 from fastapi import APIRouter, Depends
+from starlette import status
 from transliterate import translit
 
 from api.dependencies.database import get_repository
@@ -16,32 +17,32 @@ from utils import frame_maker
 router = APIRouter()
 
 
-@router.post('/video')
+@router.post('/video', status_code=status.HTTP_201_CREATED)
 async def create_video(
     user: CreateUser,
     settings: AppSettings = Depends(get_settings),
     user_repository: UserRepository = Depends(get_repository(UserRepository)),
 ):
     file_path = user.video.file_path
-    
+
     url = f'https://api.telegram.org/file/bot{settings.token}/{file_path}'
-    
+
     response = requests.get(url)
 
     uuid_value = uuid.uuid4()
-    
+
     with open("video.mp4", "wb") as file:
         file.write(response.content)
-    
+
     frame_maker.start_make_frames(
         'video.mp4',
         user_data={
             'first_name': user.first_name,
             'last_name': user.last_name,
         },
-       uuid_value=uuid_value
+        uuid_value=uuid_value,
     )
-    
+
     os.remove('video.mp4')
 
     label = f'{translit(user.first_name, language_code="ru", reversed=True)}_{translit(user.last_name, language_code="ru", reversed=True)}--{uuid_value}'
